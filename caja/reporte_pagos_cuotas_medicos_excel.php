241477dee52649604b9f3640a23be341142a0595
jkarrenoo <jkarreno@gmail.com>
2022-11-15 20:09:30 -0600

    Se corrigio el reporte de pagos cuotas servicios medicos

--- caja/reporte_pagos_cuotas_medicos_excel.php
+++ caja/reporte_pagos_cuotas_medicos_excel.php
@@ -2,44 +2,48 @@
 //Inicio la sesion 
 session_start();
 include('../conexion.php');
-include ("excelgen.class.php");
+include('../funciones.php');
 
-//initiate a instance of "excelgen" class
-$excel = new ExcelGen("ReporteCuotasMedicos");
+require '../vendor/autoload.php';
 
-//initiate $row,$col variables
-$row=0;
-$col=0;
 
-$excel->WriteText($row,$col,"N. Recibo");$col++;
-$excel->WriteText($row,$col,"Fecha");$col++;
-$excel->WriteText($row,$col,"N. Paciente");$col++;
-$excel->WriteText($row,$col,"Paciente");$col++;
-$excel->WriteText($row,$col,"Monto");$col++;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 
-$row++;
-$col=0;
+$excel = new SpreadSheet();
+$hojaActiva = $excel->getActiveSheet();
+$hojaActiva ->setTitle("ReporteCuotasMedicos");
+
+$hojaActiva->setCellValue('A1', 'N. Recibo');
+$hojaActiva->setCellValue('B1', 'Fecha');
+$hojaActiva->setCellValue('C1', 'N. Paciente');
+$hojaActiva->setCellValue('D1', 'Paciente');
+$hojaActiva->setCellValue('E1', 'Monto');
+
+$fila=2;
 
 $ResRecibos=mysqli_query($conn, "SELECT * FROM pagogastosmedicos WHERE Fecha>='".$_GET["fechaini"]."' AND Fecha<='".$_GET["fechafin"]."' ORDER BY Id DESC");
 while($RResRec=mysqli_fetch_array($ResRecibos))
 {
-    $ResPaciente=mysqli_fetch_array(mysqli_query($conn, "SELECT Id, Nombre, Apellidos FROM pacientes WHERE Id='".$RResRec["IdPaciente"]."'"));
-        
-        $excel->WriteText($row,$col,$RResRec["Id"]);$col++;
-		$excel->WriteText($row,$col,$RResRec["Fecha"]);$col++;
-		$excel->WriteText($row,$col,$ResPaciente["Id"]);$col++;
-		$excel->WriteText($row,$col,$ResPaciente["Nombre"].' '.$ResPaciente["Apellidos"]);$col++;
-		$excel->WriteNumber($row,$col,$RResRec["CantidadEntregada"]);$col++;
+    $ResPaciente=mysqli_fetch_array(mysqli_query($conn, "SELECT Id, Nombre, Apellidos, Apellidos2 FROM pacientes WHERE Id='".$RResRec["IdPaciente"]."'"));
 
-        $row++;
-		$col=0;
+    $hojaActiva->setCellValue('A'.$fila, $RResRec["Id"]);
+    $hojaActiva->setCellValue('B'.$fila, $RResRec["Fecha"]);
+    $hojaActiva->setCellValue('C'.$fila, $ResPaciente["Id"]);
+    $hojaActiva->setCellValue('D'.$fila, $ResPaciente["Nombre"].' '.$ResPaciente["Apellidos"].' '.$ResPaciente["Apellidos2"]);
+    $hojaActiva->setCellValue('E'.$fila, $RResRec["CantidadEntregada"]);
+
+    $fila++;
 }
 
 //bitacora
 mysqli_query($conn, "INSERT INTO bitacora (FechaHora, IdUser, Hizo, Datos) VALUES ('".time()."', '".$_SESSION["Id"]."', '127', '".json_encode($_GET)."')");
 
-//stream Excel for user to download or show on browser
-$excel->SendFile();
-?>
+// redirect output to client browser
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment;filename="ReporteCuotasMedicos.xlsx"');
+header('Cache-Control: max-age=0');
 
-
+$writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($excel, 'Xlsx');
+$writer->save('php://output');
+exit;
---
+++ caja/reporte_pagos_cuotas_medicos_excel_old.php
@@ -0,0 +1,45 @@
+<?php 
+//Inicio la sesion 
+session_start();
+include('../conexion.php');
+include ("excelgen.class.php");
+
+//initiate a instance of "excelgen" class
+$excel = new ExcelGen("ReporteCuotasMedicos");
+
+//initiate $row,$col variables
+$row=0;
+$col=0;
+
+$excel->WriteText($row,$col,"N. Recibo");$col++;
+$excel->WriteText($row,$col,"Fecha");$col++;
+$excel->WriteText($row,$col,"N. Paciente");$col++;
+$excel->WriteText($row,$col,"Paciente");$col++;
+$excel->WriteText($row,$col,"Monto");$col++;
+
+$row++;
+$col=0;
+
+$ResRecibos=mysqli_query($conn, "SELECT * FROM pagogastosmedicos WHERE Fecha>='".$_GET["fechaini"]."' AND Fecha<='".$_GET["fechafin"]."' ORDER BY Id DESC");
+while($RResRec=mysqli_fetch_array($ResRecibos))
+{
+    $ResPaciente=mysqli_fetch_array(mysqli_query($conn, "SELECT Id, Nombre, Apellidos FROM pacientes WHERE Id='".$RResRec["IdPaciente"]."'"));
+        
+        $excel->WriteText($row,$col,$RResRec["Id"]);$col++;
+		$excel->WriteText($row,$col,$RResRec["Fecha"]);$col++;
+		$excel->WriteText($row,$col,$ResPaciente["Id"]);$col++;
+		$excel->WriteText($row,$col,$ResPaciente["Nombre"].' '.$ResPaciente["Apellidos"]);$col++;
+		$excel->WriteNumber($row,$col,$RResRec["CantidadEntregada"]);$col++;
+
+        $row++;
+		$col=0;
+}
+
+//bitacora
+mysqli_query($conn, "INSERT INTO bitacora (FechaHora, IdUser, Hizo, Datos) VALUES ('".time()."', '".$_SESSION["Id"]."', '127', '".json_encode($_GET)."')");
+
+//stream Excel for user to download or show on browser
+$excel->SendFile();
+?>
+
+
