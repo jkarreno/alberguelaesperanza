5c05004a12a10a7318166b825c828da653f0007c
jkarrenoo <jkarreno@gmail.com>
2023-02-02 16:56:04 -0600

    Se arreglaron los registros que tenian diagnostico como no especificado

--- arregla_registros.php
+++ arregla_registros.php
@@ -3,23 +3,33 @@
 include ('conexion.php');
 
 //leer los pacientes
-$ResPacientes=mysqli_query($conn, "SELECT Id, Diagnostico1 FROM pacientes WHERE Diagnostico1!='' ");
+//$ResPacientes=mysqli_query($conn, "SELECT Id, Diagnostico1 FROM pacientes WHERE Diagnostico1!='' ");
+//
+//while($RResPac=mysqli_fetch_array($ResPacientes))
+//{
+//    //buscar en diagnosticos
+//    $RDiag=mysqli_query($conn, "SELECT * FROM diagnosticos WHERE Diagnostico='".$RResPac["Diagnostico1"]."'");
+//
+//    if(mysqli_num_rows($RDiag)==0)
+//    {
+//        mysqli_query($conn, "INSERT INTO diagnosticos (Diagnostico) VALUES ('".$RResPac["Diagnostico1"]."')");
+//
+//        $RDiag=mysqli_query($conn, "SELECT * FROM diagnosticos WHERE Diagnostico='".$RResPac["Diagnostico1"]."'");
+//    }
+//    $ResDiag=mysqli_fetch_array($RDiag);
+//
+//    //actualiza pacientes
+//    mysqli_query($conn, "UPDATE pacientes SET Diagnostico1='".$ResDiag["Id"]."' WHERE Id='".$RResPac["Id"]."'");
+//
+//    echo $RResPac["Id"].' - '.$RResPac["Diagnostico1"].' > '.$ResDiag["Id"].'<br />';
+//}
 
-while($RResPac=mysqli_fetch_array($ResPacientes))
+$ResRes=mysqli_query($conn, "SELECT * FROM `reservacion` WHERE `Diagnostico` = 218");
+while($RResRes=mysqli_fetch_array($ResRes))
 {
-    //buscar en diagnosticos
-    $RDiag=mysqli_query($conn, "SELECT * FROM diagnosticos WHERE Diagnostico='".$RResPac["Diagnostico1"]."'");
+    $ResPac=mysqli_fetch_array(mysqli_query($conn, "SELECT Instituto1, Diagnostico1 FROM pacientes WHERE Id='".$RResRes["IdPaciente"]."'"));
 
-    if(mysqli_num_rows($RDiag)==0)
-    {
-        mysqli_query($conn, "INSERT INTO diagnosticos (Diagnostico) VALUES ('".$RResPac["Diagnostico1"]."')");
+    mysqli_query($conn, "UPDATE reservacion SET Diagnostico='".$ResPac["Diagnostico1"]."' WHERE Id='".$RResRes["Id"]."'");
 
-        $RDiag=mysqli_query($conn, "SELECT * FROM diagnosticos WHERE Diagnostico='".$RResPac["Diagnostico1"]."'");
-    }
-    $ResDiag=mysqli_fetch_array($RDiag);
-
-    //actualiza pacientes
-    mysqli_query($conn, "UPDATE pacientes SET Diagnostico1='".$ResDiag["Id"]."' WHERE Id='".$RResPac["Id"]."'");
-
-    echo $RResPac["Id"].' - '.$RResPac["Diagnostico1"].' > '.$ResDiag["Id"].'<br />';
+    echo "Actualizada reservación ".$RResRes["Id"].'<br />';
 }
